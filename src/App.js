dad76dbfa858a74a789ce02134f81d5ddbff02ae
Tsquare25 <tibebe1234t@gmail.com>
2021-11-06 17:23:17 +0000

    added scroll and reset global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Row from "./components/Row";
 import requests from "./requests";
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import "./Row.css";
-const baseUrl = "https://image.tmdb.org/t/p/w500/";
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
