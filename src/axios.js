73fe34626e1aa19b6481123f40c577f71e5d4251
Tsquare25 <tibebe1234t@gmail.com>
2021-11-06 17:18:35 +0000

    fetched movie data for row component

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+// base url to make requests 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "./Row.css";
+const baseUrl = "https://image.tmdb.org/t/p/w500/";
 
-const Row = () => {
-  return <div></div>;
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get(fetchUrl);
+      setMovies(response.data.results);
+    };
+    fetchMovies();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      {/* title here */}
+      <h2>{title}</h2>
+      {/* posters */}
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={
+              isLargeRow
+                ? `${baseUrl}${movie.poster_path}`
+                : `${baseUrl}${movie.backdrop_path}`
+            }
+            alt={movie.title}
+            className={`row__poster ${isLargeRow && "row__large"}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Row;
